b5b4384198073795310cd3edde44da3a8c7b454c
Javed Khan <tuxcanfly@gmail.com>
2017-02-25 08:00:32 +0530

    use default

--- wally.go
+++ wally.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -10,9 +12,50 @@ import (
 	"path/filepath"
 )
 
-var url = flag.String("url", "https://www.bing.com/az/hprichbg/rb/ShengshanIsland_EN-US13597723185_1920x1080.jpg", "url of the wallpaper")
+var url = flag.String("url", "", "url of the wallpaper")
 var path = flag.String("path", "Pictures", "path to write the wallpaper")
 var filename = flag.String("filename", "wally.jpg", "filename to write the wallpaper")
+
+type BingResult struct {
+	Images []struct {
+		Startdate     string        `json:"startdate"`
+		Fullstartdate string        `json:"fullstartdate"`
+		Enddate       string        `json:"enddate"`
+		URL           string        `json:"url"`
+		Urlbase       string        `json:"urlbase"`
+		Copyright     string        `json:"copyright"`
+		Copyrightlink string        `json:"copyrightlink"`
+		Quiz          string        `json:"quiz"`
+		Wp            bool          `json:"wp"`
+		Hsh           string        `json:"hsh"`
+		Drk           int           `json:"drk"`
+		Top           int           `json:"top"`
+		Bot           int           `json:"bot"`
+		Hs            []interface{} `json:"hs"`
+	} `json:"images"`
+	Tooltips struct {
+		Loading  string `json:"loading"`
+		Previous string `json:"previous"`
+		Next     string `json:"next"`
+		Walle    string `json:"walle"`
+		Walls    string `json:"walls"`
+	} `json:"tooltips"`
+}
+
+func init() {
+	flag.Parse()
+	if *url == "" {
+		resp, err := http.Get("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		bing := new(BingResult)
+		json.NewDecoder(resp.Body).Decode(&bing)
+		*url = fmt.Sprintf("https://www.bing.com%s", bing.Images[0].URL)
+		log.Printf("Using default url: %v", *url)
+	}
+}
 
 func main() {
 	usr, err := user.Current()
