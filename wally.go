774b5b0077b9d45d8a36b89bea9a23b58fdd2e19
Javed Khan <tuxcanfly@gmail.com>
2017-02-28 11:02:20 +0530

    Check recent, add flag force

--- wally.go
+++ wally.go
@@ -10,11 +10,20 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"time"
 )
 
-var url = flag.String("url", "", "url of the wallpaper")
-var path = flag.String("path", "Pictures", "path to write the wallpaper")
-var filename = flag.String("filename", "wally.jpg", "filename to write the wallpaper")
+const (
+	bingdomain = "https://www.bing.com"
+	bingurl    = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+)
+
+var (
+	url      = flag.String("url", "", "url of the wallpaper")
+	path     = flag.String("path", "Pictures", "dir for wallpaper")
+	force    = flag.Bool("force", false, "force download")
+	filename = flag.String("filename", "wally.jpg", "filename for wallpaper")
+)
 
 type BingResult struct {
 	Images []struct {
@@ -42,35 +51,54 @@ type BingResult struct {
 	} `json:"tooltips"`
 }
 
-func init() {
-	flag.Parse()
-	if *url == "" {
-		resp, err := http.Get("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		bing := new(BingResult)
-		json.NewDecoder(resp.Body).Decode(&bing)
-		*url = fmt.Sprintf("https://www.bing.com%s", bing.Images[0].URL)
-		log.Printf("Using default url: %v", *url)
-	}
-}
-
-func main() {
+func absfilepath() string {
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
 	}
+	return filepath.Join(usr.HomeDir, *path, *filename)
+}
+
+func init() {
+	flag.Parse()
+	fi, err := os.Stat(absfilepath())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	recent := fi.ModTime().After(time.Now().Add(-24 * time.Hour))
+	if recent && !*force {
+		os.Exit(0)
+	}
+	if *url != "" {
+		return
+	}
+
+	resp, err := http.Get(bingurl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	bing := new(BingResult)
+	json.NewDecoder(resp.Body).Decode(&bing)
+	*url = fmt.Sprintf("%s%s", bingdomain, bing.Images[0].URL)
+
+	log.Printf("Using default url: %v", *url)
+}
+
+func main() {
 	resp, err := http.Get(*url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	out, err := os.Create(filepath.Join(usr.HomeDir, *path, *filename))
+
+	out, err := os.Create(absfilepath())
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer out.Close()
+
 	io.Copy(out, resp.Body)
 }
